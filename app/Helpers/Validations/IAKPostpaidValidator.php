ae26772ba6f180f096f5e10d473ce4ddf00c631e
Alvin Valenciano <alvinvaja@gmail.com>
2021-05-27 17:06:52 +0700

    develop check status postpaid with testing

--- app/Helpers/Validations/IAKPostpaidValidator.php
+++ app/Helpers/Validations/IAKPostpaidValidator.php
@@ -4,6 +4,12 @@ namespace IakID\IakApiPHP\Helpers\Validations;
 
 class IAKPostpaidValidator
 {
+    public static function validateCheckStatusRequest($request)
+    {
+        IAKValidationHelper::validateContentType($request);
+        IAKValidationHelper::validateContentFields($request, ['refId']);
+    }
+
     public static function validatePricelistRequest($request)
     {
         IAKValidationHelper::validateContentType($request);
--- app/Services/IAKPostpaid.php
+++ app/Services/IAKPostpaid.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Exception\RequestException;
 use IakID\IakApiPHP\Exceptions\IAKException;
 use IakID\IakApiPHP\Helpers\Formats\Url;
 use IakID\IakApiPHP\Helpers\Request\Guzzle;
+use IakID\IakApiPHP\Helpers\Request\RequestFormatter;
 use IakID\IakApiPHP\Helpers\Validations\IAKPostpaidValidator;
 use IakID\IakApiPHP\IAK;
 
@@ -17,6 +18,27 @@ class IAKPostpaid extends IAK
         parent::__construct($data);
 
         $this->url = Url::URL_POSTPAID[$this->stage];
+    }
+
+    public function checkStatus($request = [])
+    {
+        IAKPostpaidValidator::validateCheckStatusRequest($request);
+
+        $request = RequestFormatter::formatArrayKeysToSnakeCase($request);
+
+        $request = array_merge($request, [
+            'commands' => 'checkstatus',
+            'username' => $this->credential['userHp'],
+            'sign' => $this->generateSign('cs')
+        ]);
+
+        try {
+            return Guzzle::sendRequest($this->url . '/api/v1/bill/check', 'POST', $this->headers, $request);
+        } catch (ConnectException $e) {
+            throw new IAKException($e->getMessage());
+        } catch (RequestException $e) {
+            throw new IAKException($e->getMessage());
+        }
     }
 
     public function pricelist($request = [])
